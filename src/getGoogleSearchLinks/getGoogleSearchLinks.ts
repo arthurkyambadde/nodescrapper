8fdbe1d701819da1cbcf7be14084681d65ee832d
arthurkyambadde <arthurkyambadde9@gmail.com>
2023-07-11 11:56:14 +0300

    (feat): add function that googles the provided link and returns all resultant links

---
+++ src/getGoogleSearchLinks/getGoogleSearchLinks.ts
@@ -0,0 +1,23 @@
+const cheerio = require("cheerio");
+const axios = require("axios");
+
+export async function getGoogleSearchLinks(url: string) {
+  const links_data: string[] = [];
+
+  try {
+    const response = await axios.get(url);
+    const data = await response.data;
+
+    const $ = cheerio.load(data);
+    const links = $("a");
+
+    $(links).each(function (index: number, element: any) {
+      links_data.push($(element).attr("href"));
+      // console.log(links_data);
+    });
+
+    return links_data;
+  } catch (error) {
+    return error;
+  }
+}
