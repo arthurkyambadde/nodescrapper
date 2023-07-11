6dce8985602e2bd83796d519230814d8606c4b01
arthurkyambadde <arthurkyambadde9@gmail.com>
2023-07-11 11:58:37 +0300

    (feat): call all provided functions and organise them into a functional application

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log("hello world!");
+import { getCompanyFacebookLink } from "./getCompanyFacebookLink/getCompanyFacebookLink";
+import { getGoogleSearchLinks } from "./getGoogleSearchLinks/getGoogleSearchLinks";
+import { getEmailAdress } from "./getCompanyEmail/getCompanyEmail";
+
+const company: string = "dstv";
+const url = `https://www.google.com/search?q=${company}`;
+
+const expression: RegExp =
+  /(?:https?:\/\/)?(?:www\.)?(mbasic.facebook|m\.facebook|facebook|fb)\.(com|me)\/(?:(?:\w\.)*#!\/)?(?:pages\/)?(?:[\w\-\.]*\/)*([\w\-\.]*)/;
+
+(async () => {
+  try {
+    const googleResults: any = await getGoogleSearchLinks(url);
+
+    const facebookPage = await getCompanyFacebookLink(
+      googleResults,
+      expression
+    );
+
+    const email = await getEmailAdress(facebookPage);
+
+    console.log("email", email);
+  } catch (error) {
+    console.log("error:", error);
+  }
+})();
