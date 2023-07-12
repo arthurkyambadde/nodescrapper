60b1fb6e4b6a3fd661c5b470b364f197871c2bba
arthurkyambadde <arthurkyambadde9@gmail.com>
2023-07-12 15:10:00 +0300

    (feat): create a function that reads the provided company files containg company names

--- src/readCompanyName/readCompanyNames.ts
+++ src/readCompanyName/readCompanyNames.ts
@@ -1,14 +1,7 @@
 const fs = require("fs");
 
 export const readCompanyNames = (filePath: string) => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(filePath, "utf8", (err: any, data: any) => {
-      if (err) {
-        reject(err);
-      } else {
-        const companies = data.trim().split("\n");
-        resolve(companies);
-      }
-    });
-  });
+  const data = fs.readFileSync(filePath, "utf8");
+  const dataArray = data.split("\n").filter((line: any) => line.trim() !== "");
+  return dataArray;
 };
