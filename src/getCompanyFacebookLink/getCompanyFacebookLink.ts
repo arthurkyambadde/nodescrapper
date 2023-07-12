53518449bef11967f8a99be5ca729b2c2ff8d067
arthurkyambadde <arthurkyambadde9@gmail.com>
2023-07-12 15:09:23 +0300

    (refactor): move Regex expression to getCompanyFacebookLink

--- src/getCompanyFacebookLink/getCompanyFacebookLink.ts
+++ src/getCompanyFacebookLink/getCompanyFacebookLink.ts
@@ -1,15 +1,13 @@
-export async function getCompanyFacebookLink(
-  links: string[],
-  expression: RegExp
-) {
+export async function getCompanyFacebookLink(links: string[]) {
+  const expression: RegExp =
+    /(?:https?:\/\/)?(?:www\.)?(mbasic.facebook|m\.facebook|facebook|fb)\.(com|me)\/(?:(?:\w\.)*#!\/)?(?:pages\/)?(?:[\w\-\.]*\/)*([\w\-\.]*)/;
+
   for (const url of links) {
     const match = expression.test(url);
     if (match) {
       const startIndex = url.indexOf("/url?q=") + 7;
       const endIndex = url.indexOf("&sa=U&ved=");
-      const facebookLink = url.slice(startIndex, endIndex);
-
-      console.log(facebookLink);
+      const facebookLink: string = url.slice(startIndex, endIndex);
 
       return facebookLink;
     }
